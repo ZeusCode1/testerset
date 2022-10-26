8d93ad08660fe7edeb198aa6b3e8ee7f84c4200d
ZeusCode1 <105425038+ZeusCode1@users.noreply.github.com>
2022-10-26 23:57:31 +0300

    Add files via upload

---
+++ utils/messages.js
@@ -0,0 +1,13 @@
+module.exports = {
+    giveaway: "🎉🎉 **ÇEKİLİŞ** 🎉🎉",
+    giveawayEnded: "🎉🎉 **ÇEKİLİŞ BİTTİ** 🎉🎉",
+    inviteToParticipate: "Katılmak için 🎉 ile tepki verin!",
+    dropMessage: "🎉 ile ilk tepki veren siz olun!",
+    drawing: 'Kalan Zaman: {timestamp}',
+    winMessage: "Tebrikler, {winners}! **{this.prize}** kazandınız.",
+    embedFooter: "Çekiliş Zamanı Lourity",
+    noWinner: "Çekiliş iptal edildi, geçerli katılım yok.",
+    hostedBy: "Başlatan: {this.hostedBy}",
+    winners: "Kazanan(lar)",
+    endedAt: "Şu tarihte sona erdi:"
+};
